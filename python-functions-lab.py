7a02871cecfe57a8f584dee98b6d077cfc4c3c4a
David Aydin <david.aydin6@gmail.com>
2022-06-30 14:14:01 -0400

    e2: largest function that takes a list as an argument checks if its empty, if not it finds the largest number and returns that

--- python-functions-lab.py
+++ python-functions-lab.py
@@ -1,4 +1,5 @@
 # Challenge 1
+print('\n Challenge 1: \n')
 def sum_to(n):
     sum = 0
     while (n > 0):
@@ -7,3 +8,24 @@ def sum_to(n):
     return sum
 print(sum_to(6))
 print(sum_to(10))
+
+
+# Challenge 2
+print('\n Challenge 2: \n')
+def largest(list):
+    if len(list) == 0:
+        return 'You entered an emtpy list :('
+    else:
+        max = 0
+        for num in list:
+            if num > max:
+                max = num
+        return max
+print(largest([1,2,3,4,0]))
+print(largest([10,4,2,231,91,54]))
+
+
+# Challenge 3
+
+
+# Challenge 4
