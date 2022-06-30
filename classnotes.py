eeab19194d61a6fcf1f239e454f5b5217d254615
David Aydin <david.aydin6@gmail.com>
2022-06-30 09:30:59 -0400

    parameters and arguments

--- classnotes.py
+++ classnotes.py
@@ -11,3 +11,17 @@ print(result)
 
 nums = [1,3,2,6,5]
 odds = list( filter(lambda num: num % 2, nums) )
+print(odds)
+
+
+def add (a, b):
+    return a + b
+
+def sub (a, b):
+    return a - b
+
+def compute (a, b, op):
+    return op(a,b)
+
+print( compute(1,2, add))
+
